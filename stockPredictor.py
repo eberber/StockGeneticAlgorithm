b1ee5258d79c0de16e2215fb9b40b666ef412c2f
Edgar <e_berber@u.pacific.edu>
2019-09-15 00:48:12 -0700

    starting mutation

--- stockPredictor.py
+++ stockPredictor.py
@@ -7,6 +7,7 @@ import math
 
 
 ###################          FUNCTIONS                #############################
+###################          SELECTION                #############################
 
 #randomly select two chromosomes from the current generation and whichever one has a higher fitness score will be copied
 #into the next generation. You do not need to prevent chromosomes from being selected more than once.
@@ -55,158 +56,79 @@ def selectionSort(populationUsingSelection, scoredChromosomeList = []):
                 minIndex, minValue = findMin(populationUsingSelection, elitistList)
     return elitistList
 
+###################          CROSSOVER                #############################
+def selection(populationUsingSelection, populationUsingCrossover, populationSize, scoredChromosomeList, selectionAlgo, crossOverAlgo):
+    selectionList = []
+    chromosome = collections.namedtuple('scoredChromosome', 'chromosome score')
 
-def fiftyPercentChance():
-    chance = random.randint(1, 100)
-
-    if chance <= 50:
-        return 0
-    elif chance >= 51:
-        return 1
-
-def initializeChromosome():
-    mean = 0
-    standardDeviation = 1.15
-    np.random.seed
-    flag = False
-    counter = 0
-    #left values must be smaller than right on their respective day
-    chromosome=[]
-    firstDayLeftValue = 0
-    firstDayRightValue = 0
-    nextDayLeftValue = 0
-    nextDayRightValue = 0
-    recommendationValue = -1 #1 = BUY, 0 = SHORT
-
-    while flag == False:         
-        randNumber = np.random.normal(mean, standardDeviation)
-        randNumber = round(randNumber,2)
-        
-        randNumber2 = np.random.normal(mean, standardDeviation)
-        randNumber2 = round(randNumber2,2)
-
-        if counter == 0:
-            if randNumber < randNumber2:
-                firstDayLeftValue = randNumber
-                firstDayRightValue = randNumber2
-            else:
-                firstDayLeftValue = randNumber2
-                firstDayRightValue = randNumber
-        elif counter == 1:
-            if randNumber <= randNumber2:
-                nextDayLeftValue = randNumber
-                nextDayRightValue = randNumber2
-                flag = True
-            else:
-                nextDayLeftValue = randNumber2
-                nextDayRightValue = randNumber        
-                flag = True
-        else:
-            print("Error assigning chromosome values, exiting...")
-            exit()
-        counter+=1
-    
-    recommendationValue = fiftyPercentChance()
-
-    #print("####    CHROMOSOME INITIALIZED     ####")
-    chromosome.append(firstDayLeftValue)
-    chromosome.append(firstDayRightValue)
-    chromosome.append(nextDayLeftValue)
-    chromosome.append(nextDayRightValue)
-    chromosome.append(recommendationValue)
-    #print("     ",chromosome)
-    return chromosome
-
-def validateStockData(noMatch, score, stockList = [], chromosome = []):
-    counter = 0
-    flag = False
-    stockIndex = 0
-    
-    #iterate through chromosome, except last value
-    for chromoIndex in range(2):
-        #all values of the list except for the last one, need to satisfy the range
-        if chromosome[chromoIndex*2] <= stockList[stockIndex] <= chromosome[((chromoIndex*2) + 1)]:
-            #if counter= 1 then the first value of the stock data satisfy chromo curr day
-            if counter == 1:
-                #data is within range so we check if buy or short
-                if chromosome[-1] == 1:
-                    score += stockList[-1]
-                    break
-                else:
-                    #if you sell stock, then you lost money if the last value was positive in 
-                    #the stockData list since you sold at a lower cost
-                    #if you profit when you buy, you lose when you sell
-                    score += (stockList[-1] * -1)
-                    break
-            else:
-                stockIndex = 1
-                counter += 1
-                continue
-        #if any value is not within that range skip this list
-        else:
-            noMatch +=1
-            flag = True
-            return noMatch, score, flag
-    return noMatch, score, flag
-
-def selection(populationUsingSelection, populationUsingCrossover, populationSize, scoredChromosomeList, selectionAlgo):
-    nextGenList = []
     if selectionAlgo == 0:
         print("Starting Elitist \n")
-        nextGenList = selectionSort(populationUsingSelection, scoredChromosomeList)
-
+        selectionList = selectionSort(populationUsingSelection, scoredChromosomeList)
     elif selectionAlgo == 1:
         print("Starting Tournament \n")
-        nextGenList = tournamentSort(populationUsingSelection, scoredChromosomeList)
-    print(nextGenList)
-    return nextGenList
+        selectionList = tournamentSort(populationUsingSelection, scoredChromosomeList)
 
-def fitnesScore(file, populationSize, populationUsingSelection, populationUsingCrossover, selectionAlgo):
-    scoredChromosome = collections.namedtuple('scoredChromosome', 'chromosome score')
-    scoredChromosomeList = []
+    if crossOverAlgo == 0:
+        print("Starting Uniform \n")
+        for x in range(populationUsingCrossover):
+            chromosome = uniform(selectionList)
+            selectionList.append(chromosome)
+    elif crossOverAlgo == 1:
+        print("Starting K-Point \n")
+        for x in range(populationUsingCrossover):
+            crossOverAlgo = kPoint(selectionList)
+            selectionList.append(crossOverAlgo)
+    return selectionList
 
-    for chromosomeNumber in range(populationSize):
-        tempScore = 0.0
-        score = 0.0
-        tempNoMatch = 0
-        noMatch = 0
-        counter = 0  
+#Iterate over each of the 5 genes and randomly select whether to use the value from the first parent chromosome or the second parent chromosome. 
+def uniform(selectionList):
+    childChromosome = collections.namedtuple('scoredChromosome', 'chromosome score')
+    childList = []
+    chance = -1
+    score = 0.0
+    parent1 = random.randint(0, len(selectionList) - 1)
+    parent2 = random.randint(0, len(selectionList) - 1)
 
-        #create chromosome
-        chromosome =  initializeChromosome()   
+    for i in range(5):
+        chance = fiftyPercentChance()
+        if i != 4:
+            if chance == 0:
+                childList.append(selectionList[parent1][0][i])
+            elif chance == 1:
+                childList.append(selectionList[parent2][0][i])
+        else:
+            if chance == 0:
+                score = round(selectionList[parent1][1], 2)
+            elif chance == 1:
+                score = round(selectionList[parent2][1], 2)
+    #organize list    
+    for j in range(2):
+        if childList[j * 2] > childList[(j * 2) + 1]:
+            childList[j * 2] = childList[j * 2] + childList[(j*2) + 1]
+            childList[(j * 2) + 1] = childList[j * 2] - childList[(j * 2) + 1]
+            childList[j * 2] = childList[j * 2] - childList[(j * 2) + 1]
 
-        #open file
-        with open(file, "r") as f:
-            for line in f:
-                # Now you have one line of text in the variable "line" and can
-                # Convert strings to floats   
-                stockList = [ float(x) for x in line.split() ] 
-                #iterate through single list of lists
-                tempNoMatch, tempScore, flag = validateStockData(tempNoMatch, tempScore, stockList, chromosome)
-                #make sure we dont add values that did not match conditions
-                if flag != True:
-                    score += tempScore
-                tempScore = 0.0
-                noMatch += tempNoMatch
-                tempNoMatch = 0
-                counter += 1
-            
-        if noMatch == counter:
-            #no stocks in our data matched the total # of stocks we had, set default
-            score = -5000 
-        score = round(score, 3)
-        #print("       ####     FITNESS SCORE    ####\n                  ", score)
-        #package chromosomes into list of tuples
-        scoredChromosomeList.append(scoredChromosome(chromosome, score))
-    #pass current generation and percent to be chosen
-    selection(populationUsingSelection, populationUsingCrossover, populationSize, scoredChromosomeList, selectionAlgo)
-    
-def generatePopulations(populationSize, percentSelection):
-    percentSelection = percentSelection/100
-    populationUsingSelection = math.floor(populationSize * percentSelection)
-    populationUsingCrossover = populationSize - populationUsingSelection
-    return populationUsingSelection, populationUsingCrossover
+    return childChromosome(childList, score)
+#Take the first 2 genes from the first parent chromosome and the last 3 genes from the second parent chromosome to form a child chromosome. 
+def kPoint(selectionList):
+    childList = []
+    score = 0.0
+    parent1 = random.randint(0, len(selectionList) - 1)
+    parent2 = random.randint(0, len(selectionList) - 1)
+    childChromosome = collections.namedtuple('scoredChromosome', 'chromosome score')
+    for i in range(5):
+        if i < 2:
+            childList.append(selectionList[parent1][0][i])
+        elif i < 4:
+            childList.append(selectionList[parent2][0][i])
+        else:
+            score = childList.append(selectionList[parent2][1])
+    return childChromosome(childList, score)
 
+###################          MUTATION                #############################
+#def mutation():
+
+###################          INITIALIZATION                #############################
 def userInterface():    
     print("Hello! Welcome to the chromosome fitness testing lab!\n")
     ''' try:
@@ -248,16 +170,152 @@ def userInterface():
                 break
         else:
             print("Not an integer! Try again \n")
-
+    
+    while True:
+        print("Uniform: 0 \n K-Point: 1")
+        crossOverAlgo = input()
+        if (crossOverAlgo.isdigit()):
+            crossOverAlgo = int(crossOverAlgo)
+            if crossOverAlgo != 0 and crossOverAlgo != 1:
+                print("Select 0 or 1. Try again \n")
+            else:
+                break
+        else:
+            print("Not an integer! Try again \n")
     #TODO: Dont hard code
     file = "genAlgData1.txt"
     #file = "GA_debug.txt"
     #print(file)
     start = time.time()
-    fitnesScore(file, populationSize, populationUsingSelection, populationUsingCrossover, selectionAlgo)
+    fitnesScore(file, populationSize, populationUsingSelection, populationUsingCrossover, selectionAlgo, crossOverAlgo)
     end = time.time()
     print("Completion Time In Seconds: ", round(end-start, 3))
 
+def generatePopulations(populationSize, percentSelection):
+    percentSelection = percentSelection/100
+    populationUsingSelection = math.floor(populationSize * percentSelection)
+    populationUsingCrossover = populationSize - populationUsingSelection
+    return populationUsingSelection, populationUsingCrossover
+
+def fiftyPercentChance():
+    chance = random.randint(1, 100)
+
+    if chance <= 50:
+        return 0
+    elif chance >= 51:
+        return 1
+
+def randNumpyNumber():
+    mean = 0
+    standardDeviation = 1.15
+    np.random.seed
+
+    randNumber = np.random.normal(mean, standardDeviation)
+    return randNumber
+
+def initializeChromosome():
+    #left values must be smaller than right on their respective day: [currentDay1, currentDay2, nextDay, nextDay2, stockReccomendation]
+    chromosome=[]
+    recommendationValue = -1 #1 = BUY, 0 = SHORT
+
+    for i in range(3):         
+        randNumber = randNumpyNumber()
+        randNumber = round(randNumber,2)
+        
+        randNumber2 = randNumpyNumber()
+        randNumber2 = round(randNumber2,2)
+
+        if i < 1:
+            if randNumber < randNumber2:
+                chromosome.append(randNumber)
+                chromosome.append(randNumber2)
+            else:
+                chromosome.append(randNumber2)
+                chromosome.append(randNumber)
+        elif i < 2:
+            if randNumber <= randNumber2:
+                chromosome.append(randNumber)
+                chromosome.append(randNumber2)
+            else:
+                chromosome.append(randNumber2)
+                chromosome.append(randNumber)    
+        else:
+            recommendationValue = fiftyPercentChance()
+            chromosome.append(recommendationValue)
+
+    return chromosome
+
+def validateStockData(noMatch, score, stockList = [], chromosome = []):
+    counter = 0
+    flag = False
+    stockIndex = 0
+    
+    #iterate through chromosome, except last value
+    for chromoIndex in range(2):
+        #all values of the list except for the last one, need to satisfy the range
+        if chromosome[chromoIndex*2] <= stockList[stockIndex] <= chromosome[((chromoIndex*2) + 1)]:
+            #if counter= 1 then the first value of the stock data satisfy chromo curr day
+            if counter == 1:
+                #data is within range so we check if buy or short
+                if chromosome[-1] == 1:
+                    score += stockList[-1]
+                    break
+                else:
+                    #if you sell stock, then you lost money if the last value was positive in 
+                    #the stockData list since you sold at a lower cost
+                    #if you profit when you buy, you lose when you sell
+                    score += (stockList[-1] * -1)
+                    break
+            else:
+                stockIndex = 1
+                counter += 1
+                continue
+        #if any value is not within that range skip this list
+        else:
+            noMatch +=1
+            flag = True
+            return noMatch, score, flag
+    return noMatch, score, flag
+
+def fitnesScore(file, populationSize, populationUsingSelection, populationUsingCrossover, selectionAlgo, crossOverAlgo):
+    scoredChromosome = collections.namedtuple('scoredChromosome', 'chromosome score')
+    scoredChromosomeList = []
+
+    for chromosomeNumber in range(populationSize):
+        tempScore = 0.0
+        score = 0.0
+        tempNoMatch = 0
+        noMatch = 0
+        counter = 0  
+
+        #create chromosome
+        chromosome =  initializeChromosome()   
+
+        #open file
+        with open(file, "r") as f:
+            for line in f:
+                # Now you have one line of text in the variable "line" and can
+                # Convert strings to floats   
+                stockList = [ float(x) for x in line.split() ] 
+                #iterate through single list of lists
+                tempNoMatch, tempScore, flag = validateStockData(tempNoMatch, tempScore, stockList, chromosome)
+                #make sure we dont add values that did not match conditions
+                if flag != True:
+                    score += tempScore
+                tempScore = 0.0
+                noMatch += tempNoMatch
+                tempNoMatch = 0
+                counter += 1
+            
+        if noMatch == counter:
+            #no stocks in our data matched the total # of stocks we had, set default
+            score = -5000 
+        score = round(score, 3)
+        #print("       ####     FITNESS SCORE    ####\n                  ", score)
+        #package chromosomes into list of tuples
+        scoredChromosomeList.append(scoredChromosome(chromosome, score))
+    #pass current generation and percent to be chosen
+    scoredChromosomeList = selection(populationUsingSelection, populationUsingCrossover, populationSize, scoredChromosomeList, selectionAlgo, crossOverAlgo)
 
 ###################          MAIN                #############################
 if __name__ == '__main__':
